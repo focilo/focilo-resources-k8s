ffcb32ac4779b4ece2f56b2570a9da7cec17d99f
Diego Rodriguez <diego.rodriguez@cern.ch>
2017-02-13 18:11:10 +0100

    config: split multiple yaml files
    * Since config for namespaces, quotas and workers are stored in a
      single jinja template, the output was a single file with many yaml
      files inside. This commit generates a file for each yaml.
    
    Signed-off-by: Diego Rodriguez <diego.rodriguez@cern.ch>

--- scripts/build_manifests.sh
+++ scripts/build_manifests.sh
@@ -6,6 +6,20 @@ scriptpathname=$(cd "$(dirname "$0")" && pwd)
 # grab the name of the resources which need to be built
 resources=$(for child in $scriptpathname/../templates/*; do file_name=$(basename "$child"); if [ "$file_name" != "config.yaml" ]; then echo "$file_name"; fi ; done)
 
+split_file () {
+    extension=${1##*.}
+    cd "$(dirname "$1")"
+    csplit -zq "$(basename "$1")" /"$2"/ "{*}"
+    for old_file_name in xx*;
+    do
+        new_file_name="$(grep 'name' "$old_file_name" | head -1 | cut -d ':' -f 2 | tr -d ' ').$extension"
+        mv "$old_file_name" "$new_file_name"
+        echo "Created -> $(dirname "$1")/$new_file_name"
+    done;
+    rm "$config_file_path"
+    cd "$scriptpathname"
+}
+
 build_manifests () {
     mkdir -p "$scriptpathname"/../configuration_manifests/"$1"
     for template_file_path in $scriptpathname/../templates/"$1"/*template.yaml;
@@ -15,7 +29,12 @@ build_manifests () {
         config_file_path="$scriptpathname/../configuration_manifests/$config_file_name"
         jinja2 "$template_file_path" "$scriptpathname/../templates/config.yaml" > "$config_file_path"
         sed '/^[[:space:]]*$/d' -i "$config_file_path"
-        echo "'Created -> '$config_file_path"
+        if [ "$(grep -c '\-\-\-' "$config_file_path")" -gt 1 ];
+        then
+            split_file "$config_file_path" "---"
+        else
+            echo "Created -> $config_file_path"
+        fi
     done;
 }
 
