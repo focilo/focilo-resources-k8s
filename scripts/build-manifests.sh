c89b155bdec814eafb799ee0fd62cb2221bc8fd3
Diego Rodriguez <diego.rodriguez@cern.ch>
2017-02-13 18:14:28 +0100

    config: add script and templates as resources
    * In order to make `build-manifests.sh` accesible when installing the
      package, both the script and the `templates` directory have been added
      to it.
    
    Signed-off-by: Diego Rodriguez <diego.rodriguez@cern.ch>

--- scripts/build-manifests.sh
+++ scripts/build-manifests.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 # detect pathname of this script:
-scriptpathname=$(cd "$(dirname "$0")" && pwd)
+templates_path=$(echo "from __future__ import print_function; \
+import pkg_resources; \
+print(pkg_resources.resource_filename('reana_resources_k8s', \
+  'templates'))" | python)
 
 # grab the name of the resources which need to be built
-resources=$(for child in $scriptpathname/../templates/*; do file_name=$(basename "$child"); if [ "$file_name" != "config.yaml" ]; then echo "$file_name"; fi ; done)
+resources=$(for child in $templates_path/*; do file_name=$(basename "$child"); if [ "$file_name" != "config.yaml" ]; then echo "$file_name"; fi ; done)
 
 split_file () {
     extension=${1##*.}
@@ -17,17 +20,17 @@ split_file () {
         echo "Created -> $(dirname "$1")/$new_file_name"
     done;
     rm "$config_file_path"
-    cd "$scriptpathname"
+    cd "$(dirname "$1")"
 }
 
 build_manifests () {
-    mkdir -p "$scriptpathname"/../configuration_manifests/"$1"
-    for template_file_path in $scriptpathname/../templates/"$1"/*template.yaml;
+    mkdir -p "$templates_path"/../configuration_manifests/"$1"
+    for template_file_path in $templates_path/"$1"/*template.yaml;
     do
-        template_file_name=${template_file_path//"$scriptpathname/../templates/"/}
+        template_file_name=${template_file_path//"$templates_path/"/}
         config_file_name=${template_file_name//-template/}
-        config_file_path="$scriptpathname/../configuration_manifests/$config_file_name"
-        jinja2 "$template_file_path" "$scriptpathname/../templates/config.yaml" > "$config_file_path"
+        config_file_path="$templates_path/../configuration_manifests/$config_file_name"
+        jinja2 "$template_file_path" "$templates_path/config.yaml" > "$config_file_path"
         sed '/^[[:space:]]*$/d' -i "$config_file_path"
         if [ "$(grep -c '\-\-\-' "$config_file_path")" -gt 1 ];
         then
