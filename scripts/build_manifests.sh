c5f846698f543ba2f7698410084e9c4202892d48
Diego Rodriguez <diego.rodriguez@cern.ch>
2017-02-13 09:57:53 +0100

    config: organize configuration as directory tree
    Signed-off-by: Diego Rodriguez <diego.rodriguez@cern.ch>

--- scripts/build_manifests.sh
+++ scripts/build_manifests.sh
@@ -3,17 +3,23 @@
 # detect pathname of this script:
 scriptpathname=$(cd "$(dirname "$0")" && pwd)
 
-function build_reana_system_nodes_manifests {
-    mkdir -p "$scriptpathname"/../configuration_manifests/deployments/reana-system
-    for template_file_path in $scriptpathname/../templates/reana-system/*template.yaml;
+# grab the name of the resources which need to be built
+resources=$(for child in $scriptpathname/../templates/*; do file_name=$(basename "$child"); if [ "$file_name" != "config.yaml" ]; then echo "$file_name"; fi ; done)
+
+build_manifests () {
+    mkdir -p "$scriptpathname"/../configuration_manifests/"$1"
+    for template_file_path in $scriptpathname/../templates/"$1"/*template.yaml;
     do
-        template_file_name=${template_file_path//"$scriptpathname/../templates/reana-system/"/}
+        template_file_name=${template_file_path//"$scriptpathname/../templates/"/}
         config_file_name=${template_file_name//-template/}
-        config_file_path="$scriptpathname/../configuration_manifests/deployments/reana-system/$config_file_name"
+        config_file_path="$scriptpathname/../configuration_manifests/$config_file_name"
         jinja2 "$template_file_path" "$scriptpathname/../templates/config.yaml" > "$config_file_path"
         sed '/^[[:space:]]*$/d' -i "$config_file_path"
         echo "'Created -> '$config_file_path"
     done;
 }
 
-build_reana_system_nodes_manifests
+for resource in $resources;
+do
+    build_manifests "$resource";
+done;
